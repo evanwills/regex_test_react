46f15de9d0b7e92c21ced15f3d11fb5e5201f801
Evan Wills <evan.wills@acu.edu.au>
2019-01-22 08:42:46 +1100

    add interaction JS for multi line & long line control

--- tmp/regex-pair.js
+++ tmp/regex-pair.js
@@ -1,5 +1,7 @@
-function autoResize () {
-  var main = $(this).parent().parent()
+var hasTextarea = {}
+var isLongLine = {}
+
+function resizeTextarea (main) {
   var lines = 3
   var maxHeight = 15
   var regex = new RegExp('(?:\r\n|\n\r|\r|\n)', 'g')
@@ -33,21 +35,92 @@ function autoResize () {
     } else {
       $(main).find('textarea').removeAttr('style')
     }
-    console.log(lines)
+    // console.log(lines)
   })
+}
+
+function autoResizeLongLine () {
+  // console.log('inside autoResizeLongLine()')
+  resizeTextarea($(this))
+}
+
+function autoResizeMultiLine () {
+  var main = $(this).parent().parent()
+  // console.log('inside autoResizeMultiLine()')
+  $(main).find('textarea').each(function () {
+    resizeTextarea($(this))
+  })
+}
+
+function getID(element) {
+  var id = $(element).attr('id')
+  return id.replace(/^.*?--([0-9])__.*$/, '$1');
+}
+
+function longLine () {
+  var doLongLine = $(this).is(':checked')
+  var isMulti = false
+  var id = getID($(this))
+  var prefix = 'regex-pair--'
+  var ID = '#' + prefix + id
+
+  isMulti = hasTextarea[ID];
+
+  isLongLine[ID] = doLongLine;
+  if (doLongLine === true) {
+    $(ID).addClass(prefix + 'long-line').removeClass(prefix + 'multi-line')
+    $(ID).off('keyup').on('keyup', autoResizeLongLine)
+  } else {
+    $(ID).removeClass(prefix + 'long-line')
+    $(ID).off('keyup').on('keyup', autoResizeMultiLine)
+    if (isMulti) {
+      $(ID).addClass(prefix + 'multi-line')
+    }
+  }
+}
+
+function modifiersMultiLine () {
+  var id = 'regex-pair--' + getID($(this))
+  var wasTextarea = false
+  var val = $(this).val()
+  var ID = '#' + id
+
+  wasTextarea = hasTextarea[id]
+  if (val.match('m')) {
+    if (wasTextarea === false) {
+      hasTextarea[id] = true
+      // convert find & replace input fields to textarea
+      console.log('should now be textarea')
+
+      // add on keyup listener for appropriate layout (isLong)
+      if (isLongLine[id] === true) {
+        $(ID + ' textarea').on('keyup', autoResizeLongLine)
+      } else {
+        $(ID + ' textarea').on('keyup', autoResizeMultiLine)
+      }
+      $(ID + ' textarea').trigger('change')
+    }
+  } else {
+    if (wasTextarea === true) {
+      // remove on keyup listener
+      $(ID + ' textarea').off('keyup')
+
+      hasTextarea[id] = false
+
+      // convert find & replace textarea fields to input
+    }
+  }
 }
 
 $(document).ready(function () {
   'use strict'
   var extraOpen = {}
 
-  console.log('document is ready')
   $('.regex-pair__open-close').on('click', function () {
-    var ID = $(this).data('id')
+    var ID = getID($(this))
     var extraID = '#regex-pair--' + ID + '__extra'
     var tabIndex = 1
-    // var isOpen = $(this).hasClass('regex-pair__extra--open');
-    console.log('extraID:', extraID)
+
     if (typeof extraOpen[ID] === 'undefined') {
       extraOpen[ID] = true
     }
@@ -64,19 +137,32 @@ $(document).ready(function () {
       $('#regex-pair--' + ID + '__delimiter').focus()
     }
   })
+  $('.regex-pair').each(function () {
+    var id = $(this).attr('id')
+    hasTextarea[id] = false
+    isLongLine[id] = $(this).hasClass('regex-pair--long-line')
+  })
+  $('textarea').each(function () {
+    var id = 'regex-pair--' + getID($(this));
+    hasTextarea[id] = true
+  })
   $('.regex-pair__open-close--open').trigger('click')
   $('.regex-pair--multi-line .regex-pair__input--find').on('focus', function (e) {
-    console.log('focused')
     $(this).parent().parent().find('.regex-pair__modifiers').addClass('regex-pair__modifiers--blured')
   })
 
   $('.regex-pair--multi-line .regex-pair__input--find').on('blur', function (e) {
-    console.log('blurred')
     $(this).parent().parent().find('.regex-pair__modifiers').removeClass('regex-pair__modifiers--blured')
   })
+  $('.regex-pair__input--modifiers').on('blur', modifiersMultiLine)
 
-  // $('.regex-pair--multi-line').autoResize()
+  $('input.regex-pair__longLine').on('change', longLine)
+  $('.regex-pair--multi-line textarea').on('keyup', autoResizeMultiLine)
+  $('.regex-pair--long-line textarea').on('keyup', autoResizeLongLine)
 
-  $('.regex-pair--multi-line textarea').on('keyup', autoResize)
-  $('.regex-pair--multi-line textarea').trigger('keyup')
+  // $('.regex-pair--multi-line textarea').trigger('keyup')
+  // $('.regex-pair--long-line textarea').trigger('keyup')
+  $('input.regex-pair__longLine').trigger('change')
+  $('.regex-pair__main textarea').trigger('keyup')
+  $('.regex-pair__input--modifiers').trigger('blur')
 })
